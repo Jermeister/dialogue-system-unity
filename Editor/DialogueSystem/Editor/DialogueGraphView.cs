c7cf98d08a66a196bbc6f99c78526761078baac3
Lina Gudžiūnaitė <ascendingman@gmail.com>
2021-07-07 22:47:48 +0300

    [Package] Initialized package structure

--- Editor/DialogueSystem/Editor/DialogueGraphView.cs
+++ Editor/DialogueSystem/Editor/DialogueGraphView.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
-using Unity.Profiling;
 using UnityEditor;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
